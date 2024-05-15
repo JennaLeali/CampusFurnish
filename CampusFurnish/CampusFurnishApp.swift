382b37977588c064a1901b44c4d893ec9e59fe62
Jenna Leali <jleali2021@fau.edu>
2024-05-15 14:47:30 -0400

    Initial Commit

---
+++ CampusFurnish/CampusFurnishApp.swift
@@ -0,0 +1,17 @@
+//
+//  CampusFurnishApp.swift
+//  CampusFurnish
+//
+//  Created by Jenna Leali on 5/15/24.
+//
+
+import SwiftUI
+
+@main
+struct CampusFurnishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
